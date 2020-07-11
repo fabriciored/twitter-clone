59a1b8eb8410b656f7f296e984ad80aab64412ea
fabriciored <fabriciojlsantos@gmail.com>
2020-07-11 17:43:32 -0300

    finished Header component

--- src/components/Main/styles.js
+++ src/components/Main/styles.js
@@ -16,6 +16,7 @@ export const Container = styled.div `
 `;
 
 export const Header = styled.div `
+  z-index: 2; 
   position: sticky;
   top: 0;
   background: var(--primary);
@@ -48,4 +49,17 @@ export const BackIcon = styled(ArrowLeft) `
   fill: var(--twitter);
 `;
 
-export const ProfileInfo = styled.div ``;
+export const ProfileInfo = styled.div `
+  margin-left: 17px;
+  display: flex;
+  flex-direction: column;
+
+  > strong {
+      font-size: 19px;
+  }
+  > span {
+      font-size: 15px;
+      color: var(--gray);
+  }
+
+`;
